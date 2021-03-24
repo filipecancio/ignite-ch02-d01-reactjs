bb216420d392643c48a34e90b4a05560d0913689
Filipe Câncio <filipe.cancio@outlook.com>
2021-03-23 22:14:54 -0300

    Update index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MdAddShoppingCart } from 'react-icons/md';
+import { MdAddShoppingCart } from 'react-icons/md'; 
 
 import { ProductList } from './styles';
 import { api } from '../../services/api';
